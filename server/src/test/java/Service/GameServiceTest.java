bb59dbfa9787ee82225f6bca826a46851f5264c3
nbn1343 <nbn1343@gmail.com>
2024-02-27 13:34:30 -0700

    Completed final GameServiceTests

--- server/src/test/java/Service/GameServiceTest.java
+++ server/src/test/java/Service/GameServiceTest.java
@@ -10,6 +10,8 @@ import model.GameData;
 import org.junit.jupiter.api.*;
 import service.GameService;
 
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
@@ -25,24 +27,66 @@ class GameServiceTest {
     gameService = new GameService (gameDAO, authDAO);
   }
 
-//  @Test
-//  void successfulCreateGame() throws DataAccessException {
-//    AuthData authData = new AuthData("validAuthToken", "username");
-//    authDAO.createAuth(authData);
-//
-//    // Create a game
-//    GameData gameID = gameService.createGame("whiteUser", "blackUser", "Chess", null, "validAuthToken");
-//
-//    // Check if the game was successfully created
-//    assertNotNull(gameDAO.getGame(gameID.gameID ()), "Game not created successfully");
-//  }
-//
-//  @Test
-//  void createGameWithInvalidAuthToken() {
-//    // Attempt to create a game with an invalid authToken
-//    assertThrows(DataAccessException.class, () ->
-//                    gameService.createGame("whiteUser", "blackUser", "Chess", null, "invalidAuthToken"),
-//            "Creating game with invalid authToken should throw DataAccessException");
-//  }
+  @Test
+  void successfulCreateGame() throws DataAccessException {
+    AuthData authData = new AuthData("validAuthToken", "username");
+    authDAO.createAuth(authData);
+    GameData gameID = gameService.createGame(1,"whiteUser", "blackUser", "Chess", null, "validAuthToken");
+    assertNotNull(gameDAO.getGame(gameID.gameID ()), "Game not created successfully");
+  }
+
+  @Test
+  void createGameWithInvalidAuthToken() {
+    assertThrows(DataAccessException.class, () ->
+                    gameService.createGame(1,"whiteUser", "blackUser", "Chess", null, "invalidAuthToken"),
+            "Creating game with invalid authToken should throw DataAccessException");
+  }
+
+  @Test
+  void successfulJoinGame() throws DataAccessException {
+    AuthData authData = new AuthData("validAuthToken", "username");
+    authDAO.createAuth(authData);
+    GameData gameData = gameService.createGame(1, "whiteUser", null, "Chess", null, "validAuthToken");
+    gameService.joinGame("validAuthToken", gameData.gameID(), "BLACK");
+    GameData updatedGameData = gameDAO.getGame(gameData.gameID());
+    assertNotNull(updatedGameData);
+    assertEquals("username", updatedGameData.blackUsername());
+  }
+
+  @Test
+  void joinGameWithInvalidAuthToken() {
+    assertThrows(DataAccessException.class, () ->
+                    gameService.joinGame("invalidAuthToken", 1, "BLACK"),
+            "Joining game with invalid authToken should throw DataAccessException");
+  }
+
+  @Test
+  void joinGameWithInvalidGameID() {
+    assertThrows(DataAccessException.class, () ->
+                    gameService.joinGame("validAuthToken", 999, "BLACK"),
+            "Joining non-existent game should throw DataAccessException");
+  }
+
+  @Test
+  void successfulListGames() throws DataAccessException {
+    AuthData authData = new AuthData("validAuthToken", "username");
+    authDAO.createAuth(authData);
+
+    gameService.createGame(1, "whiteUser1", "blackUser1", "Chess1", null, "validAuthToken");
+    gameService.createGame(2, "whiteUser2", "blackUser2", "Chess2", null, "validAuthToken");
+
+    Collection<GameData> games = gameService.listGames("validAuthToken");
+
+    assertFalse(games.isEmpty(), "List of games should not be empty");
+
+    assertEquals(2, games.size(), "Incorrect number of games listed");
+  }
+
+  @Test
+  void listGamesWithInvalidAuthToken() {
+    assertThrows(DataAccessException.class, () ->
+                    gameService.listGames("invalidAuthToken"),
+            "Listing games with invalid authToken should throw DataAccessException");
+  }
 }
 
